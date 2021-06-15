3e9f1bd364a4162884e636dded1e8a94466d30cc
SL <ip_adress@gmx.de>
2021-06-15 23:03:49 +0200

    next step

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# my test
+
+[mkdocs](https://www.mkdocs.org/getting-started/#installationa)
--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 docker build -t mkdocs .
 
-if [ -d "`pwd`:/docs/" 
-docker run --rm -ti -p8000:8000 -v `pwd`:/docs
+if [ ! -d "`pwd`/docs/myprj" ];then
+   docker run -w /docs/ -v `pwd`/docs:/docs --rm -ti mkdocs sh -c 'mkdocs new myprj'
+fi
+docker run -p8000:8000 -w /docs/myprj -v `pwd`/docs:/docs --rm -ti mkdocs sh -c 'mkdocs serve --dev-addr=0.0.0.0:8000'
 
